930c347a91b1be2f9059c09def680de4a6dacab2
tandisSh <tandis00shojaee@gmail.com>
2024-11-27 14:13:57 +0330

    git init

---
+++ footer.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="footer" >
+    <section class="content-footer">
+        <div class="top-footer">
+            <div class="logo-footer">
+                <a href="/" class="router-link">
+                    <h2 class="font" style="font-weight:bold;">فروشگاه الینور</h2>
+                </a>
+                <h5>با عضویت در خبر نامه الینور از آخرین خبر ها با خبر شوید</h5>
+            </div>
+                <div class="subscrib-email">
+                    <form class="email-form">
+                    <input id="email-form-input" type="email" placeholder="ایمیل">
+                        <button class="send-email">عضویت</button>
+                    </form>
+                </div>
+        </div>
+               <div class="down-footer">
+                     <div class="down-right">
+                        <h4 class="font" style="font-size: 18px; font-weight:bold;" >درباره فروشگاه الینور</h4>
+                        <p class="line2"> فروشگاه الینور ابتدا در زمستان سال ۱۳۹۸ در قالب یک فروشگاه کوچک اینترنتی در اینستاگرام آغاز بکار کرد یـک سـال بعـد در خـرداد مـاه سـال ۱۳۹۹ اولـین فـروشـگاه حضـوری و وبسایت رسمی الینور با.شعار “همیشه ... </p>
+                        <a href="#" class="more__button" style="margin-bottom:10px;">ادامه......</a>
+
+                    <div class="flex">
+                        <div class="socialMedia">
+                            <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="#e35f83" class="bi bi-instagram" viewBox="0 0 16 16">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                                </svg>
+                                    </a>
+                                    <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="#e35f83" class="bi bi-pinterest" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0" />
+                            </svg>
+                                     </a>
+                                     <a href="">
+                                         <svg aria-hidden="true" focusable="false" role="presentation" class="icon" width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                             <path fill="#e35f83" d="M0.0468725 0L7.47201 9.92802L0 18H1.68166L8.22336 10.9329L13.5089 18H19.2316L11.3887 7.51356L18.3436 0H16.6619L10.6373 6.50867L5.76958 0H0.0468725ZM2.51982 1.23871H5.14884L16.7582 16.7611H14.1292L2.51982 1.23871Z"></path>
+                                             </svg>
+                                      </a>
+                                      <a href="">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" class="icon" viewBox="0 0 24 24" id="facebook">
+                                              <path fill="#e35f83" d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z" data-v-331744f8=""></path>
+                                              </svg>
+                                              </a>
+                                              </div>
+                          </div>
+                          </div>
+
+                  <div class="down-left">
+                      <div >
+                          <h4 class="font2" style="font-size: 18px;">دسته بندی</h4>
+                          <ul class="menu-fuuter">
+                              <li>
+                                  <a target="_self" href="#">لباس زنانه</a>
+                               </li>
+                            <li>
+                                <a target="_self" href="#">ست لباس زنانه</a>
+                            </li>
+                                 <li>
+                                <a target="_self" href="#">مانتو</a>
+                                 </li>
+                             <li>
+                                 <a target="_self" href="#">شلوار زنانه</a>
+                             </li>
+                                 <li>
+                                  <a target="_self" href="#">کراپ</a>
+                                 </li>
+                              </ul>
+                              
+                              </div>
+                              
+                              <div>
+                                  <h4 class="font2" style="font-size: 18px;">دسته بندی</h4>
+                                <ul class="menu-fuuter">
+                                    <li>
+                                        <a target="_self" href="#">بامبرجکت زنانه</a>
+                                    </li>
+                                        <li>
+                                            <a target="_self" href="#">بافت و پلیور زنانه</a>
+                                        </li>
+                                     <li>
+                                         <a target="_self" href="#">کت زنانه</a>
+                                     </li>
+                                        <li>
+                                            <a target="_self" href="#">شکت زنانه</a>
+                                        </li>
+                                     <li>
+                                         <a target="_self" href="#">بلوز زنانه</a>
+                                     </li>
+                                 </ul>
+                               </div>
+                               
+                        <div>
+                            <h4 class="font2" style="font-size: 18px;">دسترسی سریع </h4>
+                            <ul class="menu-fuuter">
+                                <li>
+                                    <a href="#"target="_self">قوانین و مقررات</a>
+                                </li>
+                                     <li>
+                                    <a href="#" target="_self">پشتیبانی</a>
+                                     </li>
+                                <li>
+                                    <a href="#" target="_self">ضمانت مرجوعی</a>
+                                </li>
+                                     <li>
+                                         <a href="#" target="_self">حریم خصوصی</a>
+                                     </li>
+                                <li>
+                                    <a target="_blank" href="#">پیگیری مرسوله</a>
+                                </li>
+                            </ul>
+                         </div>
+                   </div>
+               </div>
+        <section class="down-about">
+            <div class="address">
+            <ul class="d-flex"style="padding-right: 0px !important; gap: 6px;">
+                <li class="d-flex">
+                    <span class="" style="white-space: nowrap;"> شماره تماس : </span>
+                    <a href="#" class="font-800" style="font-weight:bold;">1732431259</a>
+                </li>
+              <li class="line-address"> | </li>
+              
+              <li class="d-flex">
+                  <span style="white-space: nowrap;"> آدرس فروشگاه: </span>
+                    <span class="font-800" style="font-weight:bold;">گلستان – گرگان – بلوار ناهارخوران – نبش  عدالت 58 – فروشگاه الینور</span>
+               </li>
+             </ul>
+         </div>
+      
+         <div class="d-flex enamd-box" style="gap: 16px;">
+             <a target="_blank" href="#">
+                    <img style="width:110px; height:110px; justify-content:flex-end;" src="http://www.p30web.org/wp-content/uploads/enamad/enamad_icon_text_color_blue_275.png" alt="Alternate Text" />
+             </a>
+         </div>
+      </section>
+
+         <div class="footer__bottom">
+             <div class="footer__bottom__copyright">
+                <a href="/" class="router-link">کلیه حقوق این وب سایت به الینور تعلق دارد</a>
+            </div>
+          <div class="footer__bottom__english__copyright">
+                <a href="/">Copyright elinorboutique.ir Designed By <b>shetabit</b></a>
+            </div>
+         </div>
+      </section>
+  </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="header">
+    <section class="header">
+        <div class="header-top">
+            <a href="#" id="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="116.776" height="35.882" viewBox="0 0 116.776 35.882">
+                    <path id="Logo" d="M50.34,35.882a8.24,8.24,0,0,1,1.347-2.212C54.134,29.9,54.235,30.5,58.535,30.5q6.731,0,13.463,0l-2.792,4.025c-.654.933-.653,1.25-1.827,
+                        1.357ZM0,32.592c.383-.272-.236.032.566-.317a2.607,2.607,0,0,1,.657-.225c5.619-1.68,5.537-5.427,9.617-12.6C18.085,6.715,13.889,8.672,24,8.672c-1.4,2.7-2.7,
+                        5.264-4.191,7.977-1.435,2.621-2.57,5.313-4.06,7.812-1.7,2.852-2.086,5.382-5.11,7.04A13.153,13.153,0,0,1,4.292,32.65C2.924,32.65,1.482,32.592,0,32.592Zm22.828,
+                        0c.373-.265-.221.026.569-.316.081-.035.341-.126.431-.157,2.808-.964,3.75-1.349,5.439-4.61-2.514,0-9.366,1.032-9.366-3.289,0-3.355,1.581-4.729,3.639-8.243,4.772-8.148,
+                        4.909-7.9,13.629-7.9H47.706c3.535-6.825,4.815-5.382,10.829-5.382a26.622,26.622,0,0,1-1.64,2.51c-2.768,4.279-2.183,3.171-9.189,3.171C47.282,10.234,37.6,25.58,37.169,27.21c2.222,
+                        0,5.646.207,7.33-.285,6.132-1.79,7.311-6.45,10.58-11.617,1.042-1.647,1.7-3.25,3.325-4.377,2.078-1.445,2.737-1.371,5.983-1.363,4.878.012,9.757,0,14.634,0a12.987,12.987,0,0,1-1.659,
+                        3.389c-1.914,3.664-4.3,6.53-2.646,10.278a5.956,5.956,0,0,0,2.256,2.795c2.4,1.562,3.65-1.286,4.883-3.4L92.468,4.469c.559-.954,1.17-2.149,1.753-2.993C95.462-.318,98.739.3,102.435.3l-4.81,
+                        8.243c-1.648,2.8-3.075,5.234-4.722,8.033-1.662,2.823-3.147,5.544-4.817,8.236-2.039,3.288-5.509,2.1-9.944,2.1-2.918,0-5.749.646-8.022-.475a6.037,6.037,0,0,1-3.392-5.207c0-5.333,3.944-8.122,
+                        4.683-11.362-2.846,0-3.919-.033-5.572,1.185-1.8,1.323-4.146,5.671-5.256,7.786-2.413,4.6-5.424,8.055-10.865,8.635-3.524.376-9.1.037-12.842.037-3.044,4.643-4.5,5.113-9.485,5.113C26.126,32.623,
+                        24.632,32.593,22.829,32.593Zm12.217-22.5c-1.334,1.211-7.827,11.653-7.827,13.233,0,1.647.965,3.446,2.634,3.589.3-1.133,9.553-16.641,10.536-18.539C37.588,8.373,36.559,8.716,35.045,10.09ZM0,32.592H0ZM90.143,
+                        27.21v-.3c4.424,0,5.748-3.285,7.444-6.148l9.654-17.047c1.493-2.6,1.7-3.462,2.955-3.715h6.581c-1.169,2.256-12.8,23.875-15.038,25.6-1.86,1.432-4.1,1.691-6.739,1.691C93.507,27.293,91.888,27.21,90.143,27.21Z" fill="#F05454"></path></svg>
+            </a>
+            <form id="search-box">
+                <div id="search-main">
+                    <div id="glass-input">
+                        <button class="icon" type="submit">
+                            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786
+                            18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                            </button>
+                        <input class="search-input" type="text" placeholder="چی میخوای؟!">
+                    </div>
+                </div>
+            </form>
+            <div class="header_buttons">
+                <button class="cart__button">
+                    <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.761 20.8538C9.5904 19.5179 7.57111 17.9456 5.73929
+                      16.1652C4.45144 14.8829 3.47101 13.3198 2.8731 11.5954C1.79714 8.25031 3.05393 4.42083 6.57112 3.28752C8.41961 2.69243 10.4384 3.03255 11.9961 4.20148C13.5543 3.03398 15.5725 2.69398 17.4211 3.28752C20.9383 4.42083 22.2041 8.25031 21.1281
+                      11.5954C20.5302 13.3198 19.5498 14.8829 18.2619 16.1652C16.4301 17.9456 14.4108 19.5179 12.2402 20.8538L12.0051 21L11.761 20.8538Z" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                       </path><path d="M15.7393 7.05301C16.8046 7.39331 17.5615 8.34971 17.6561 9.47499" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                </button>
+                <button title="سبد خرید" class="cart__button"><svg  xmlns="http://www.w3.org/2000/svg" width="31.203" height="31.203" viewBox="0 0 31.203 31.203">
+                    <g id="Bag_Icon-2" data-name="Bag Icon"><path id="Vector" d="M0,7.361V6.1A6.205,6.205,0,0,1,5.278.028,5.851,5.851,0,0,1,11.7,5.853V7.647" transform="translate(9.751 2.611)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" opacity="0.8"></path><path id="Vector-2" data-name="Vector" d="M7.477,18.2h7.8c5.226,0,6.163-2.093,6.436-4.641l.975-7.8C23.039,2.587,22.129,0,16.578,0H6.177C.625,0-.285,2.587.066,5.76l.975,7.8C1.314,16.108,2.25,18.2,7.477,18.2Z"
+                        transform="translate(4.224 10.401)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path><path id="Vector-3" data-name="Vector" d="M.495.5H.506" transform="translate(19.651 15.101)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" opacity="0.8"></path><path id="Vector-4" data-name="Vector" d="M.495.5H.506" transform="translate(10.549 15.101)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" 
+                        stroke-width="2" opacity="0.8"></path><path id="Vector-5" data-name="Vector" d="M0,0H31.2V31.2H0Z" fill="none" opacity="0"></path></g></svg><!----></button>
+                <button id="login-btn" class="cart__button title="پروفایل کاربری" > ورود و عضویت </button>
+            </div>
+        </div>
+        <div id="header-down">
+            <div class="header-menu">
+                <nav class="menu"> 
+                    <ul class="main-menu">
+                     <li class="menu-item">
+                         <span>
+                             <a href="#" class="dropdown">زنانه</a>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="9.2" height="5.26" viewBox="0 0 9.2 5.26" class="arrow me-2"><path id="Icon_ionic-ios-arrow-down"
+                                    data-name="Icon ionic-ios-arrow-down" d="M10.789,14.921l3.479-3.481a.655.655,0,0,1,.929,0,.663.663,0,0,1,0,.931l-3.941,3.944a.656.656,0,0,1-.907.019L6.379,12.374a.658.658,0,1,1,.929-.931Z"
+                                    transform="translate(-6.188 -11.246)" fill="currentColor"></path></svg>
+                         </span>
+                     
+                     </li>
+                        <li class="menu-item">
+                            <span>
+                                <a href="#">وبلاگ</a>
+                            </span>
+                        </li>
+                        <li class="menu-item">
+                            <span>
+                                <a href="#">درباره ما</a>
+                            </span>
+                        </li>
+                        <li class="menu-item">
+                            <span>
+                                <a href="#">تماس با ما</a>
+                            </span>
+                        </li>
+                        <li class="menu-item">
+                            <span>
+                                <a href="#">ضمانت مرجوعی</a>
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </section>
+
+</div>
+</body>
+</html>
