ab53a3a9a6ecbda32179d1a6d569f63d1644924f
elmira-prli <elmira00pourali@gmail.com>
2024-12-14 09:54:16 +0330

    fix errror lougout in dashbord

--- pages/vendor/Userlogout.php
+++ pages/vendor/Userlogout.php
@@ -1,9 +1,10 @@
 <?php 
 session_start();
-unset($_SESSION['user']);
+// unset($_SESSION['user']);
+// session_destroy();
+$_SESSION = []; // حذف تمامی متغیرهای موجود در جلسه
+session_unset(); // حذف متغیرهای جلسه
 session_destroy();
 header("location:../login.php");
 exit();
-
-
 ?>
