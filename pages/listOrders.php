50f8a52667abc31c3fd21103b1ee1985bebd0a28
elmira-prli <elmira00pourali@gmail.com>
2024-12-18 00:52:50 +0330

    fix css logout

--- pages/listOrders.php
+++ pages/listOrders.php
@@ -10,7 +10,7 @@ $result = $pdo->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>لیست محصولات</title>
-    <link rel="stylesheet" href="../public/css/listOrder.css">
+    <link rel="stylesheet" href="../public/css/listProducts.css">
 </head>
 <body>
     <div class="container" >
